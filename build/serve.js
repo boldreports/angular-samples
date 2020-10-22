1e083c19346d1bdb569f367244775387d2aec843
Subhasree Vasudevan <subhasree.vasudevan@syncfusion.com>
2020-10-22 15:58:03 +0530

    feature(REPORT-5087): Resolved memory issue

--- build/serve.js
+++ build/serve.js
@@ -1,6 +1,9 @@
-var gulp = require("gulp");
-const shelljs = require('shelljs');
+const gulp = require("gulp");
+const { exec } = require('shelljs');
+
+const ngCli = "node --max_old_space_size=4096 node_modules/@angular/cli/bin/ng";
+
 
 gulp.task('serve', ['generate-router'], () => {
-  shelljs.exec('ng serve --open');
+  exec(`${ngCli} serve --open`);
 });
