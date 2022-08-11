fca26c51c9fbabb5f013e36bea77b506aa37c1fa
sundaresan.gunasekaran <sundaresan.gunasekaran@syncfusion.com>
2022-08-11 12:10:04 +0530

    feature(REPORT-17597): Bold-reports 4.2 release changes

--- build/build.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const { exec } = require('shelljs');
-const argv = require('yargs').argv;
-
-const ngCli = "node --max_old_space_size=4096 node_modules/@angular/cli/bin/ng";
-
-gulp.task('build', ['generate-router'], function () {
-  exec(`${ngCli} build`);
-});
