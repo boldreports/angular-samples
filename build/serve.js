2b81d0046ac015f5c35081b32aeb10f508cfda11
Vishnu7101 <vishnu.tamilventhan@syncfusion.com>
2025-02-10 17:49:32 +0530

    feature(REPORT-405059): Bold Reports 7.1 Changes

--- build/serve.js
+++ build/serve.js
@@ -5,7 +5,7 @@ const ngCli = "node --max_old_space_size=4096 node_modules/@angular/cli/bin/ng";
 const runSequence = require('gulp4-run-sequence');
 
 gulp.task('serve', (done) => {
-  runSequence('update-barcode', 'generate-router', () => {
+  runSequence('update-extensions-export', 'generate-router', () => {
     exec(`${ngCli} serve --open --port ${argv.port || ''}`);
     done();
   });
