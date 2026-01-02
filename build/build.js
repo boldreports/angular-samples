452ea34911e5f330f944915e512154c601719c3e
SundaresanGunasekaran <sundaresan.gunasekaran@syncfusion.com>
2026-01-02 09:44:16 +0530

    feature(REPORT-481519): Gulp Task Updated

--- build/build.js
+++ build/build.js
@@ -7,7 +7,7 @@ const ngCli = "node --max_old_space_size=4096 node_modules/@angular/cli/bin/ng";
 
 gulp.task('production-build', (done) => {
   runSequence('update-extensions-export', 'generate-router', () => {
-    if (argv.prefix) {
+    if (argv?.prefix) {
       exec(`${ngCli} build -c production --base-href ${argv.prefix}`);
     } else {
       exec(`${ngCli} build -c production`);
